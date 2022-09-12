4e9ed1f20d989f38d9b9bff9aa3f1f2ed1956632
AJANI ISMAILA ABIODUN <ismaheelben@gmail.com>
2022-09-12 09:38:19 -0700

    &&&&5

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,20 +7,25 @@
 
 int main(void)
 {
-		int d1, d2, n = 0;
+		int d1, d2, d3, n = 0;
 
 		for (d1 = '0'; d1 <= '9'; d1++)
 					{
-		for (d2 = d1 + 1; d2 <= '9'; d2++, n++)
-		{
-		if (n > 0)
-													{
-																putchar(',');
-																		putchar(' ');
+		for (d2 = d1 + 1; d2 <= '9'; d2++)
+					{
+			for (d3 = d2 + 1; d3 <= '9'; d3++, n++)
+			{
+				if (n > 0)
+																	{
+																				putchar(',');
+																						putchar(' ');
+																							}
+
+																putchar(d1);
+																	putchar(d2);
+																		putchar(d3);
 																			}
-												putchar(d1);
-													putchar(d2);
-														}
+												}
 								} putchar('\n');
 			return (0);
 }
