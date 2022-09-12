117fdaf16327a0c2c1518e565f87d27670d8489e
AJANI ISMAILA ABIODUN <ismaheelben@gmail.com>
2022-09-12 09:54:39 -0700

    finally

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,16 +2,17 @@
 
 
 /**
-* main - entry
-*
-* Return: 0 if successful (always)
-*/
+ * main - entry
+ *
+ * Return: 0 if successful (always)
+ */
 
 int main(void)
 {
-	char number = '0';
-		for (number = '0'; number <= '9'; number++)
-		putchar(number);
-		putchar('\n');
-		return (0);
-}              
+		char number = '0';
+
+				for (number = '0'; number <= '9'; number++)
+				putchar(number);
+				putchar('\n');
+				return (0);
+}
