6f7f65d493469df3cc0fd9cba1a452d732d431a6
AJANI ISMAILA ABIODUN <ismaheelben@gmail.com>
2022-09-13 14:14:26 -0700

    6666&

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include<stdio.h>
+
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,3 +18,4 @@ void print_to_98(int n);
 void print_times_table(int n);
 void print_times_table(int n);
 
+#endif /* MAIN_H */
