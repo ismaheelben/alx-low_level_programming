d04f0d4ef47e96150e26adb824a23779e7251175
AJANI ISMAILA ABIODUN <ismaheelben@gmail.com>
2022-09-13 07:45:41 -0700

    &&555

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point task2
+ * Return:0 success
+ */
+
+void print_alphabet(void)
+{
+		char c = 'a';
+
+			while (c <= 'z')
+					{
+								_putchar(c);
+										c++;
+
+											}
+				_putchar('\n');
+
+}
