5ea8cbc0d2a6978fbcf06cf0854576dab5cdb9b2
AJANI ISMAILA ABIODUN <ismaheelben@gmail.com>
2022-09-13 07:05:27 -0700

    &&&

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,19 +2,19 @@
 #include "main.h"
 /**
  *  main - Prints _putchar task 0
- * Return: Always 0 (success) 
+ *  Return: Always 0 (success) 
  */
 int main(void)
 }
-		 _putchar( '_' ) ; 
-		 _putchar( 'p' ) ; 
-		 _putchar( 'u' ) ; 
-		 _putchar( 't' ) ; 
-		 _putchar( 'c' ) ; 
-		 _putchar( 'h' ) ; 
-		 _putchar( 'a' ) ; 
-		 _putchar( 'r' ) ; 
-		 _putchar( '\n' ) ; 
+		 _putchar('_');
+		 _putchar('p');
+		 _putchar('u');
+		 _putchar('t');
+		 _putchar('c');
+		 _putchar('h');
+		 _putchar('a');
+		 _putchar('r');
+		 _putchar('\n'); 
 		 return (0) ;
 
 }
