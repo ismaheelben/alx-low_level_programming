3339a0570b0b626b6a73a181ea69669153dcc62e
AJANI ISMAILA ABIODUN <ismaheelben@gmail.com>
2022-09-13 08:05:39 -0700

    &&&55

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * 3islower - checks for lowercase task3
+ *
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+int _islower(int c)
+{
+		return (c >= 'a' && c <= 'z');
+}
