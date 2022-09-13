459ac0de2d9cb93dd10651faa4aca13810e32a50
AJANI ISMAILA ABIODUN <ismaheelben@gmail.com>
2022-09-13 07:34:11 -0700

    55&&

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,20 @@
-#include <unistd.h>
 #include "main.h"
+
 /**
- *  main - Prints _putchar task 0
- *  Return: Always 0 success
+ * main -  prints _putchar, followed by a new line task0
+ * Return: Always 0 (Success)
  */
-int _putchar(char c)
+int main(void)
 {
-		return (write(1, &c, 1));
+		_putchar('_');
+			_putchar('p');
+				_putchar('u');
+					_putchar('t');
+						_putchar('c');
+							_putchar('h');
+								_putchar('a');
+									_putchar('r');
+										_putchar('\n');
+
+											return (0);
 }
