976442cf573e44eb5e17a7455a093438dadae555
AJANI ISMAILA ABIODUN <ismaheelben@gmail.com>
2022-09-21 07:48:04 -0700

    mainheafer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 /**
  *main - header file for protoypes
  *void protoypes (void)
  *int protoypes (int)
  */
-
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
