6c262803f7738ae4e248ff5954a7a3439a4dde04
AJANI ISMAILA ABIODUN <ismaheelben@gmail.com>
2022-09-23 04:49:14 -0700

    task

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -22,5 +22,6 @@ char *leet(char *);
 char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, char *r, int r_index);
+_putchar;
 
 #endif /*MAIN_H*/
