fd22b48866b1dc857e69ad1902f2c826e066dad2
AJANI ISMAILA ABIODUN <ismaheelben@gmail.com>
2022-09-26 12:10:04 -0700

    tasky

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,13 +6,13 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	int i, s;
+	int i, j;
 
 	for (i = 0; i < 8; i++)
 	{
-		for (j = 0; s < 8; s++)
+		for (j = 0; j < 8; j++)
 		{
-			_putchar(a[i][s]);
+			_putchar(a[i][j]);
 		}
 		_putchar('\n');
 	}
