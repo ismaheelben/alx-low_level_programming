f958beea6fe0d4b2a1eb9663d3227810b4bfc7ee
AJANI ISMAILA ABIODUN <ismaheelben@gmail.com>
2022-09-26 11:42:07 -0700

    header

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,13 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <studio.h>
-/**
- *main - header file for protoypes
- *void protoypes (void)
- *int protoypes (int)
- *char prototypes (strcat)
- *Author ismaila 
- */
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
@@ -20,4 +12,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
-#endif /*MAIN_H*/
+#endif
