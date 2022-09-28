c9c6c3e4a5cd7e0ee05672c0ff3798a24221e5a8
AJANI ISMAILA ABIODUN <ismaheelben@gmail.com>
2022-09-28 07:21:20 -0700

    task

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print and check
+ * ismaila
+ * Return: On success (1) on error return (-1)
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+
+}
