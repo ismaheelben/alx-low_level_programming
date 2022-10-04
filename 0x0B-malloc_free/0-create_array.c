3138f0ae08f7d0772463931388fdfa8a5c544150
AJANI ISMAILA ABIODUN <ismaheelben@gmail.com>
2022-10-03 23:45:52 -0700

    hwadee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - chars, and initializes it with a specific char
+ * a specific char
+ * @size: size of an array
+ * @c: char to initialize array with
+ * author ismail
+ * Return: NULL if it fails or pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	arr = malloc(sizeof(char) * size);
+
+	/* check if malloc was successful */
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
